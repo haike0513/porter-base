06cf984a858de9cd7819a886cd69b480ef41142b
haike <haike0513@gmail.com>
2025-10-03 07:58:36 +0800

    add ping

--- porter-cli/src/main.rs
+++ porter-cli/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use iroh::{protocol::Router, Endpoint, Watcher};
+use iroh_ping::{ALPN as PingALPN, Ping};
+#[tokio::main]
+async fn main() -> Result<()> {
+    let recv_ep = Endpoint::builder().discovery_n0().bind().await?;
+    let recv_router = Router::builder(recv_ep)
+        .accept(PingALPN, Ping::new())
+        .spawn();
+    let addr = recv_router.endpoint().node_addr().get().unwrap();
+
+    // create a send side & send a ping
+    let send_ep = Endpoint::builder().discovery_n0().bind().await?;
+    let send_pinger = Ping::new();
+    let rtt = send_pinger.ping(&send_ep, addr).await?;
+    println!("ping took: {rtt:?} to complete");
+    Ok(())
 }
